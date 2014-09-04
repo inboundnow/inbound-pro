27fc769cb5ee26fc2b4ac09c5b2f9a48c267c09e
Inbound Now <david@inboundnow.com>
2014-09-03 19:25:11 -0700

    Don't call PHP magic class functions statically

--- core/cta/classes/class.menus.php
+++ core/cta/classes/class.menus.php
@@ -2,19 +2,19 @@
 
 class CTA_Menus {
 
-	public static function __construct() {
+	public function __construct() {
 		self::load_hooks();
 	}
-	
+
 	public static function load_hooks() {
 		add_action('admin_menu', array( __CLASS__ , 'add_sub_menus' ) );
 	}
-	
+
 	public static function add_sub_menus() {
 		if ( !current_user_can('manage_options')) {
 			return;
 		}
-	
+
 		add_submenu_page('edit.php?post_type=wp-call-to-action', __( 'Forms' , 'cta' ), __( 'Manage Forms' , 'cta' ) , 'manage_options', 'inbound-forms-redirect',100);
 
 		add_submenu_page('edit.php?post_type=wp-call-to-action', __( 'Templates' , 'cta' ) , __( 'Manage Templates' , 'cta' ) , 'manage_options', 'wp_cta_manage_templates','wp_cta_manage_templates',100);
@@ -24,7 +24,7 @@ class CTA_Menus {
 		/* Add settings page for frontend editor */
 		add_submenu_page('edit.php?post_type=wp-call-to-action', __( 'Editor' , 'cta' ), __( 'Editor' , 'cta' ) , 'manage_options', 'wp-cta-frontend-editor', 'wp_cta_frontend_editor_screen');
 	}
-	
+
 }
 
 $CTA_Menus = new CTA_Menus();
