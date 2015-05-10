b75546abc3d9c847606f9c7c14f962d0ad3e7f87
Hudson Atwell <hudson.atwell@gmail.com>
2015-05-10 01:21:45 -0500

    Adding bootstrap tooltip support https://github.com/inboundnow/inbound-pro/issues/53

--- classes/admin/class.settings.php
+++ classes/admin/class.settings.php
@@ -66,10 +66,11 @@ class Inbound_Pro_Settings {
 		wp_localize_script('inbound-settings', 'inboundSettingsLoacalVars' ,  array('apiURL' => Inbound_API_Wrapper::get_api_url() , 'siteURL' => site_url() ) );
 
 		/* load Ink */
-		wp_enqueue_script('Ink-holder', INBOUND_PRO_URLPATH . 'assets/libraries/Ink/js/holder.js' );
-		wp_enqueue_script('Ink-all', INBOUND_PRO_URLPATH . 'assets/libraries/Ink/js/ink-all.min.js' );
-		wp_enqueue_script('Ink-autoload', INBOUND_PRO_URLPATH . 'assets/libraries/Ink/js/autoload.min.js' );
 		wp_enqueue_style('Ink', INBOUND_PRO_URLPATH . 'assets/libraries/Ink/css/ink-flex.min.css');
+
+		/* Load selective bootstrap */
+		wp_enqueue_style('bootstrap-tooltip', INBOUND_PRO_URLPATH . 'assets/libraries/BootStrap/css/tooltip.min.css');
+		wp_enqueue_script('bootstrap-tooltip', INBOUND_PRO_URLPATH . 'assets/libraries/BootStrap/js/tooltip.min.js');
 
 		/* load fontawesome */
 		wp_enqueue_style('fontawesome', INBOUND_PRO_URLPATH . 'assets/libraries/FontAwesome/css/font-awesome.min.css');
@@ -531,7 +532,7 @@ class Inbound_Pro_Settings {
 				echo '		<input type="text" name="'.$field['id'].'" id="'.$field['id'].'" placeholder="'.( isset( $field['placeholder'] ) ? $field['placeholder'] : '' ) .'"  value="'.$field['value'].'" size="30"  data-field-type="'.$field['type'].'" data-field-group="'.$group['group_name'].'"/>';
 				echo '	</div>';
 				echo '	<div class="inbound-tooltip-field">';
-				echo '		<i class="tooltip fa fa-question-circle tool_text" title="'.$field['description'].'"></i>';
+				echo '		<i class="inbound-tooltip fa fa-question-circle tool_text" title="'.$field['description'].'"></i>';
 				echo '	</div>';
 				echo '</div>';
 				BREAK;
@@ -568,7 +569,7 @@ class Inbound_Pro_Settings {
 			// textarea
 			case 'textarea':
 				echo '<textarea name="'.$field['id'].'" id="'.$field['id'].'" cols="106" rows="6"  data-field-type="'.$field['type'].'" data-field-group="'.$group['group_name'].'">'.$field['value'].'</textarea>
-						<i class="tooltip fa-question-circle tool_textarea" title="'.$field['description'].'"></i>';
+						<i class="inbound-tooltip fa-question-circle tool_textarea" title="'.$field['description'].'"></i>';
 				break;
 			// wysiwyg
 			case 'wysiwyg':
@@ -581,7 +582,7 @@ class Inbound_Pro_Settings {
 				echo '<label for="upload_image">';
 				echo '<input name="'.$field['id'].'"	id="'.$field['id'].'" type="text" size="36" name="upload_image" value="'.$field['value'].'"  data-field-type="'.$field['type'].'" data-field-group="'.$group['group_name'].'"/>';
 				echo '<input class="upload_image_button" id="uploader_'.$field['id'].'" type="button" value="Upload Image" />';
-				echo '<br /><i class="tooltip fa-question-circle tool_media" title="'.$field['description'].'"></i>';
+				echo '<br /><i class="inbound-tooltip fa-question-circle tool_media" title="'.$field['description'].'"></i>';
 				break;
 			// checkbox
 			case 'checkbox':
@@ -606,7 +607,7 @@ class Inbound_Pro_Settings {
 					$i++;
 				}
 				echo "</table>";
-				echo '<br><i class="tooltip fa-question-circle tool_checkbox" title="'.$field['description'].'"></i>';
+				echo '<br><i class="inbound-tooltip fa-question-circle tool_checkbox" title="'.$field['description'].'"></i>';
 			break;
 			// radio
 			case 'radio':
@@ -626,7 +627,7 @@ class Inbound_Pro_Settings {
 				echo '	</div>';
 
 				echo '	<div class="inbound-tooltip-field">';
-				echo '		<br /><i class="tooltip fa fa-question-circle tool_dropdown" title="'.$field['description'].'"></i>';
+				echo '		<br /><i class="inbound-tooltip fa fa-question-circle tool_dropdown" title="'.$field['description'].'"></i>';
 				echo '	</div>';
 				echo '</div>';
 
@@ -649,13 +650,13 @@ class Inbound_Pro_Settings {
 				echo '		</select>';
 				echo '	</div>';
 				echo '	<div class="inbound-tooltip-field">';
-				echo '		<br /><i class="tooltip fa fa-question-circle tool_dropdown" title="'.$field['description'].'"></i>';
+				echo '		<br /><i class="inbound-tooltip fa fa-question-circle tool_dropdown" title="'.$field['description'].'"></i>';
 				echo '	</div>';
 				echo '</div>';
 			break;
 			case 'html':
 				echo $field['value'];
-				echo '<br /><i class="tooltip fa-question-circle tool_dropdown" title="'. ( isset($field['description'] ) ? $field['description'] : '' ) .'"></i>';
+				echo '<br /><i class="inbound-tooltip fa-question-circle tool_dropdown" title="'. ( isset($field['description'] ) ? $field['description'] : '' ) .'"></i>';
 			break;
 			case 'custom-fields-repeater':
 				$fields = Leads_Field_Map::get_lead_fields();
