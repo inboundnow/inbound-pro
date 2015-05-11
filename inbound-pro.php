606d779f591680cdd910aaabdad8b065ab11c536
Hudson Atwell <hudson.atwell@gmail.com>
2015-05-11 14:57:49 -0500

    testing increment

--- inbound-pro.php
+++ inbound-pro.php
@@ -4,7 +4,7 @@ Plugin Name: Inbound Pro
 Plugin URI: http://www.inboundnow.com/
 Description: Pro Version of Inbound Now Plugins
 Author: Inbound Now
-Version: 1.0.0
+Version: 1.0.2
 Author URI: http://www.inboundnow.com/
 Text Domain: inbound-pro
 Domain Path: /lang/
@@ -93,7 +93,7 @@ if ( !class_exists('Inbound_Pro_Plugin')	) {
 		*/
 		private static function define_constants() {
 
-			define('INBOUND_PRO_CURRENT_VERSION', '1.0.0' );
+			define('INBOUND_PRO_CURRENT_VERSION', '1.0.2' );
 			define('INBOUND_PRO_URLPATH', WP_PLUGIN_URL.'/'.plugin_basename( dirname(__FILE__) ).'/' );
 			define('INBOUND_PRO_PATH', WP_PLUGIN_DIR.'/'.plugin_basename( dirname(__FILE__) ).'/' );
 			define('INBOUND_PRO_SLUG', plugin_basename( dirname(__FILE__) ) );
