d552028442c137433e8835f8bbce8c4cdd2b980f
davidwells <david@inboundnow.com>
2015-12-13 18:39:44 -0800

    add sync script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,7 +1,15 @@
-cp -rf ../cta core/cta && rm -rf core/cta/shared
-cp -rf core/cta/shared core/shared
-rm -rf core/cta/shared
-cp -rf ../landing-pages core/landing-pages && rm -rf core/landing-pages/shared
-cp -rf ../leads core/leads && rm -rf core/leads/shared
-cp -rf ../inbound-mailer core/inbound-mailer
-cp -rf ../inbound-automation core/inbound-automation
+#!/bin/bash
+#rsync -a ../cta/* ./core/cta
+cp -rf ../cta ./core
+cp -rf ./core/cta/shared ./core
+rm -rf ./core/cta/shared
+echo "cta synced"
+cp -rf ../leads ./core && rm -rf ./core/leads/shared
+echo "leads synced"
+cp -rf ../landing-pages ./core && rm -rf ./core/landing-pages/shared
+echo "landing pages synced"
+cp -rf ../inbound-mailer ./core
+echo "mailer synced"
+cp -rf ../inbound-automation ./core
+echo "automation synced"
+echo "sync done"
