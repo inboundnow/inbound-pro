6859b21c859ee447d3200de10cd0f7eea39c4f1d
GITUSER <hudson@inboundnow.com>
2016-06-06 19:52:43 -0500

    Bumped version number to 1.7.1.4.2

--- core/landing-pages/classes/class.statistics.php
+++ core/landing-pages/classes/class.statistics.php
@@ -40,13 +40,7 @@ if (!class_exists('Landing_Pages_Stats')) {
         public static function record_impression($post_id, $post_type , $variation_id = 0) {
 
             /* ignore mshots and previews from admin area */
-            if (strstr( $_SERVER['HTTP_REFERER'] , 'edit.php?post_type=landing-page' )) {
-                return;
-            }
-            if (strstr( $_SERVER['HTTP_REFERER'] , 'post.php' )) {
-                return;
-            }
-            if (strstr( $_SERVER['HTTP_REFERER'] , 'edit.php' )) {
+            if (strstr( $_SERVER['HTTP_REFERER'] , admin_url() )) {
                 return;
             }
 
