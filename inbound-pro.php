2af74516e425acb88df60f77359e76f1692e02eb
GITUSER <hudson@inboundnow.com>
2017-01-29 18:11:30 -0600

    Committing uncommitted changes

--- inbound-pro.php
+++ inbound-pro.php
@@ -4,7 +4,7 @@ Plugin Name: Inbound Now PRO
 Plugin URI: http://www.inboundnow.com/
 Description: Professional Inbound Marketing Suite for WordPress
 Author: InboundWP LLC
-Version: 1.8.2.1.1
+Version: 1.8.2.1.2
 Author URI: http://www.inboundnow.com/
 Text Domain: inbound-pro
 Domain Path: /lang/
@@ -94,8 +94,8 @@ if ( !class_exists('Inbound_Pro_Plugin')	) {
 		*/
 		private static function define_constants() {
 
-			define('INBOUND_PRO_CURRENT_VERSION', '1.8.2.1.1' );
-			define('INBOUND_PRO_STABLE_VERSION', '1.8.2.1.1' );
+			define('INBOUND_PRO_CURRENT_VERSION', '1.8.2.1.2' );
+			define('INBOUND_PRO_STABLE_VERSION', '1.8.2.1.2' );
 			define('INBOUND_PRO_TRANSLATIONS_VERSION', '1.29.1' );
 			define('INBOUND_PRO_URLPATH', plugin_dir_url( __FILE__ ));
 			define('INBOUND_PRO_PATH', plugin_dir_path( __FILE__ ) );
