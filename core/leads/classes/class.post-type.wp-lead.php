5e3e39eafbf1e535b92d2dda8d21c43b8a4d993a
GITUSER <hudson@inboundnow.com>
2017-04-06 00:11:14 -0500

    Updating core

--- core/leads/classes/class.post-type.wp-lead.php
+++ core/leads/classes/class.post-type.wp-lead.php
@@ -61,7 +61,7 @@ class Leads_Post_Type {
         /* enqueue scripts and styles in admin  */
         add_action('admin_enqueue_scripts', array(__CLASS__, 'enqueue_admin_scripts'));
 
-        /* delete lead events whenlead deleted */
+        /* delete events when lead deleted */
         add_action('delete_post' , array( __CLASS__ , 'delete_lead_stats'));
 
     }
