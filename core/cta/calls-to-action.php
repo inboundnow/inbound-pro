70e33ec723ea2a09949314b97acd60789ec0ee24
GITUSER <hudson@inboundnow.com>
2017-06-16 18:13:48 -0500

    Committing uncommitted changes

--- core/cta/calls-to-action.php
+++ core/cta/calls-to-action.php
@@ -3,7 +3,7 @@
 Plugin Name: Calls to Action
 Plugin URI: http://www.inboundnow.com/cta/
 Description: Display Targeted Calls to Action on your WordPress site.
-Version: 3.2.2
+Version: 3.2.3
 Author: Inbound Now
 Author URI: https://www.inboundnow.com/
 Text Domain: cta
