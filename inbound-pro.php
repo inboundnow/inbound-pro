9cc3fd664ee349a3a593c3088e350a476de91cb5
GITUSER <hudson@inboundnow.com>
2018-03-14 06:24:36 -0500

    Preparing release

--- inbound-pro.php
+++ inbound-pro.php
@@ -4,7 +4,7 @@ Plugin Name: Inbound Now PRO
 Plugin URI: http://www.inboundnow.com/
 Description: Professional Inbound Marketing Suite for WordPress
 Author: InboundWP LLC
-Version: 1.8.6.7
+Version: 1.8.7
 Author URI: http://www.inboundnow.com/
 Text Domain: inbound-pro
 
@@ -96,8 +96,8 @@ if ( !class_exists('Inbound_Pro_Plugin')	) {
 		*/
 		private static function define_constants() {
 
-			define('INBOUND_PRO_CURRENT_VERSION', '1.8.6.7' );
-			define('INBOUND_PRO_STABLE_VERSION', '1.8.4.6' );
+			define('INBOUND_PRO_CURRENT_VERSION', '1.8.7' );
+			define('INBOUND_PRO_STABLE_VERSION', '1.8.6.7' );
 			define('INBOUND_PRO_TRANSLATIONS_VERSION', '1.30.17' );
 			define('INBOUND_PRO_URLPATH', plugin_dir_url( __FILE__ ));
 			define('INBOUND_PRO_PATH', plugin_dir_path( __FILE__ ) );
