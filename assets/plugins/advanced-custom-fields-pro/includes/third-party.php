d50e54f44f82e045619069035641b7da94500593
GITUSER <hudson@inboundnow.com>
2018-09-19 17:55:48 -0500

    Updating ACF Pro to  5.7.3

--- assets/plugins/advanced-custom-fields-pro/includes/third-party.php
+++ assets/plugins/advanced-custom-fields-pro/includes/third-party.php
@@ -31,37 +31,53 @@ class acf_third_party {
 	function __construct() {
 		
 		// Tabify Edit Screen - http://wordpress.org/extend/plugins/tabify-edit-screen/
-		add_action('admin_head-settings_page_tabify-edit-screen', array($this, 'admin_head_tabify'));
-		
+		if( class_exists('Tabify_Edit_Screen') ) {
+			add_filter('tabify_posttypes',			array($this, 'tabify_posttypes'));
+			add_action('tabify_add_meta_boxes',		array($this, 'tabify_add_meta_boxes'));
+		}
 		
 		// Post Type Switcher - http://wordpress.org/extend/plugins/post-type-switcher/
-		add_filter('pts_allowed_pages', array($this, 'pts_allowed_pages'));
+		if( class_exists('Post_Type_Switcher') ) {
+			add_filter('pts_allowed_pages', array($this, 'pts_allowed_pages'));
+		}
 		
+		// Event Espresso - https://wordpress.org/plugins/event-espresso-decaf/
+		if( function_exists('espresso_version') ) {
+			add_filter('acf/get_post_types', array($this, 'ee_get_post_types'), 10, 2);
+		}
+		
+		// Dark Mode
+		if( class_exists('Dark_Mode') ) {
+			add_action('doing_dark_mode', array($this, 'doing_dark_mode'));
+		}
 	}
 	
 	
-	/*
-	*  admin_head_tabify
+	/**
+	*  acf_get_post_types
 	*
-	*  description
+	*  EE post types do not use the native post.php edit page, but instead render their own.
+	*  Show the EE post types in lists where 'show_ui' is used.
 	*
-	*  @type	action (admin_head)
-	*  @date	9/10/12
-	*  @since	3.5.1
+	*  @date	24/2/18
+	*  @since	5.6.9
 	*
-	*  @param	n/a
-	*  @return	n/a
+	*  @param	array $post_types
+	*  @param	array $args
+	*  @return	array
 	*/
 	
-	function admin_head_tabify() {
+	function ee_get_post_types( $post_types, $args ) {
 		
-		// remove ACF from the tabs
-		add_filter('tabify_posttypes',			array($this, 'tabify_posttypes'));
+		if( !empty($args['show_ui']) ) {
+			$ee_post_types = get_post_types(array('show_ee_ui' => 1));
+			$ee_post_types = array_keys($ee_post_types);
+			$post_types = array_merge($post_types, $ee_post_types);
+			$post_types = array_unique($post_types);
+		}
 		
-		
-		// add acf metaboxes to list
-		add_action('tabify_add_meta_boxes',		array($this, 'tabify_add_meta_boxes'));
-		
+		// return
+		return $post_types;
 	}
 	
 	
@@ -171,7 +187,23 @@ class acf_third_party {
 		// return
 		return $pages;
 		
-	}	
+	}
+	
+	/**
+	*  doing_dark_mode
+	*
+	*  Runs during 'admin_enqueue_scripts' if dark mode is enabled
+	*
+	*  @date	13/8/18
+	*  @since	5.7.3
+	*
+	*  @param	void
+	*  @return	void
+	*/
+	
+	function doing_dark_mode() {
+		wp_enqueue_style('acf-dark', acf_get_url('assets/css/acf-dark.css'), array(), ACF_VERSION );
+	}
 	
 }
 
