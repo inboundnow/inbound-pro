de3603bd1abcd9573e72f4af409c568adbf1cc03
GITUSER <hudson@inboundnow.com>
2018-11-20 19:12:43 -0600

    Preparing release

--- core/mailer/classes/class.post-type.inbound-email.php
+++ core/mailer/classes/class.post-type.inbound-email.php
@@ -338,6 +338,11 @@ class Inbound_Mailer_Post_Type {
 			/* get wordpress date format */
 			$wordpress_date_time_format = (get_option('date_format') == 'd/m/Y') ?  'd/m/Y G:i' : 'm/d/Y G:i';
 
+			/* add date if does not exist */
+			if(!$settings['send_datetime']) {
+				$settings['send_datetime'] = date_i18n($wordpress_date_time_format);
+			}
+
 			/* add time if does not exist */
 			if(!strstr($settings['send_datetime'],':')) {
 				$settings['send_datetime'] = $settings['send_datetime'] . " 00:00";
