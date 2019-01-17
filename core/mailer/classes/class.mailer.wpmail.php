9cd21d78e650b9c677b6636b293e0fa234a62f55
GITUSER <hudson@inboundnow.com>
2019-01-16 20:43:31 -0600

    developing wp_mail. re:tracking pixel

--- core/mailer/classes/class.mailer.wpmail.php
+++ core/mailer/classes/class.mailer.wpmail.php
@@ -16,17 +16,50 @@ class Inbound_Mailer_WPMail extends Inbound_Mail_Daemon {
         $to = self::$email['send_address'];
         $subject = self::$email['subject'];
         $body = self::$email['body'];
+        $body = self::generate_tracking_pixel($body);
         $headers = array('Content-Type: text/html; charset=UTF-8');
 
         $result = wp_mail( $to, $subject, $body, $headers );
 
         if ($result) {
-            self::$response = array('message'=>__('success','inbound-pro'));
+            self::$response = array(
+                'message'=> 'success',
+                'email' => self::$email
+            );
         } else {
-            self::$response = array('message'=>__('fail','inbound-pro'));
+            self::$response = array(
+                'message'=> 'fail',
+                'email' => self::$email
+            );
         }
 
-        do_action( 'mailer/wp_mail/send' , self::$email  );
+        do_action( 'mailer/wp_mail/send' , self::$response , (array) self::$email , (array) self::$row  );
     }
 
+    /**
+     * Generates & appends Tracking Pixel HTML
+     * @param $body
+     * @return string
+     */
+    public static function generate_tracking_pixel( $body ) {
+
+        $params = (array) self::$row;
+
+        /* unset unneeded keys */
+        unset($params['tokens']);
+        unset($params['datetime']);
+
+        /* set action for processing */
+        $params['action'] = 'wpmail_open';
+
+        $tracking_url = add_query_arg( $params , site_url());
+
+        $tracking_html = "<img src='{$tracking_url}' width=0 height=0>";
+
+        return $body.$tracking_html;
+    }
+
+
+
 }
+
