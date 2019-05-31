8479142a04bb6c111a294f97de2c6ddd85923470
GITUSER <hudson@inboundnow.com>
2019-05-31 15:19:40 -0500

    Preparing release

--- core/landing-pages/landing-pages.php
+++ core/landing-pages/landing-pages.php
@@ -3,7 +3,7 @@
 Plugin Name: Landing Pages
 Plugin URI: http://www.inboundnow.com/landing-pages/
 Description: Landing page template framework with variant testing and lead capturing through cooperation with Inbound Now's Leads plugin. This is the stand alone version served through WordPress.org. 
-Version: 2.7.6
+Version: 2.7.7
 Author: Inbound Now
 Author URI: https://www.inboundnow.com/?innercity=true
 Text Domain: inbound-pro
@@ -42,7 +42,7 @@ if (!class_exists('Inbound_Landing_Pages_Plugin')) {
 		*/
 		private static function load_constants() {
 
-			define('LANDINGPAGES_CURRENT_VERSION', '2.7.6' );
+			define('LANDINGPAGES_CURRENT_VERSION', '2.7.7' );
 			define('LANDINGPAGES_URLPATH', plugins_url( '/' , __FILE__ ) );
 			define('LANDINGPAGES_PATH', WP_PLUGIN_DIR.'/'.plugin_basename( dirname(__FILE__) ).'/' );
 			define('LANDINGPAGES_PLUGIN_SLUG', 'landing-pages' );
